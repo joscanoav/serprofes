4412b073907988474cc7a17c6976efe7f7f20831
joscanoav <joscanoav@gmail.com>
2026-05-07 21:47:14 +0200

    update

--- 20260507/01-ejercicio/script.js
+++ 20260507/01-ejercicio/script.js
@@ -8,10 +8,15 @@ function calcularIMC() {
     // let imc = peso / (altura ** 2 ); 
     let imc = peso / (altura * altura);
     
-    // RETO DE HOY
-    // MULTIPLES DECISIONES
-    // imc < 18.5 = Estas bajo de peso
-    // imc >= 18.5 && imc <= 24.9 = Peso saludable
-    // imc >= 25 sobrepeso
-
+    //MULTIPLES DECISIONES
+    if (imc < 18.5) {
+        etiqueta.textContent = "Tu IMC es " + imc.toFixed(1) + ". Eres un esqueleto 🩻";
+        etiqueta.style.color = "blue";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        etiqueta.textContent = "Tu IMC es " + imc.toFixed(1) + ". Estas en la linea 😁";
+        etiqueta.style.color = "green";
+    } else {
+        etiqueta.textContent = "Tu IMC es " + imc.toFixed(1) + ". DANGER!! 🐷"
+        etiqueta.style.color = "red";
+    }
 }
