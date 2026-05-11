ba94bb02d1cdf19f37bd279a369513363c8ac902
joscanoav <joscanoav@gmail.com>
2026-05-11 21:49:55 +0200

    update

---
+++ 20260511/02-ejercicio/script.js
@@ -0,0 +1,4 @@
+//1. Creamos nuestra "Caja grande" (Array)
+let carrito = ["🍏 Manzana","🍌Plátano"];
+//2.Imprimir el carrito nada mas al cargar la página
+document.getElementById('pantalla-cesta').textContent = carrito.join(" - ");
