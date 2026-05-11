17e84a827274a79db6968cdd8138baa1e425c03f
joscanoav <joscanoav@gmail.com>
2026-05-11 21:57:49 +0200

    update

--- 20260511/02-ejercicio/script.js
+++ 20260511/02-ejercicio/script.js
@@ -2,3 +2,15 @@
 let carrito = ["🍏 Manzana","🍌Plátano"];
 //2.Imprimir el carrito nada mas al cargar la página
 document.getElementById('pantalla-cesta').textContent = carrito.join(" - ");
+
+//3. La función para agregar cosas nuevas a la lista
+function añadirAlCarrito(){
+    //Atrapamos lo que escribe el usuario
+    let item = document.getElementById('nuevo-item').value;
+    // MAGIA DE ARRAYS: .push() mete el nuevo elemento al final de la lista
+    carrito.push(item);
+    //Volvemos a pintar la cesta para que se vea
+    document.getElementById('pantalla-cesta').textContent = carrito.join(" - ");
+    //Limpiamos el input
+    document.getElementById('nuevo-item').value = "";
+}
