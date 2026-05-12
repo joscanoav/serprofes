d3fc1bf8892e4278c3ce6b567cff6d1ebf09d36d
joscanoav <joscanoav@gmail.com>
2026-05-12 21:29:41 +0200

    update

--- 20260512/01-ejercicio/script.js
+++ 20260512/01-ejercicio/script.js
@@ -9,4 +9,20 @@ let producto = {
 
 //2. ¿Cómo leemos un dato concreto y lo mandmos
 // al html?
-document.getElementById("prod-nombre").textContent=producto.nombre; 
+document.getElementById("prod-nombre").textContent=producto.nombre;
+document.getElementById("prod-precio").textContent=producto.precio;
+document.getElementById("prod-cat").textContent=producto.categoria; 
+
+// MINI RETO //
+function mostrarMiFicha(){
+    //1. Crea tu propio objeto "alumno"
+    let alumno = {
+        nombre: "Jorgito",
+        edad: 40,
+        ciudad:"Lima",
+    };
+    //2. Mostrar los datos en html
+    document.getElementById("alum-nombre").textContent=alumno.nombre;
+    document.getElementById("alum-edad").textContent=alumno.edad;
+    document.getElementById("alum-ciudad").textContent=alumno.ciudad;
+}
