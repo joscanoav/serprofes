c76408759473e9220726660098dbd0e044903c94
joscanoav <joscanoav@gmail.com>
2026-05-14 21:59:03 +0200

    update

--- saas-ia/app.js
+++ saas-ia/app.js
@@ -1,3 +1,34 @@
+// 1. NUESTRA BASE DE DATOS MOCK
+let historialChat = [
+    { rol: "ia", texto: "¡Hola! Soy IA Master. ¿En qué te ayudo?"},
+    { rol: "usuario", texto: "Quiero aprender JavaScript"},
+    { rol: "ia", texto: "¡Excelente elección! Empezaremos por los Arrays."}
+];
+
+//2. LA FUNCION PINTORA (Visual)
+function pintarChat(listaMensajes){
+    let caja = document.getElementById('caja-mensajes');
+    caja.innerHTML = "";
+    // MATENEMOS EL BUCLE FOR QUE APRENDIMOS AYER
+    for(let i = 0; 1 < listaMensajes.length;i++){
+        let claseCSS = listaMensajes[i].rol === "usuario" ? "msg-usuario" : "msg-ia";
+        caja.innerHTML += `<div class = "${claseCSS}"><b>
+        ${listaMensajes[i].rol.toUpperCase()}:</b>
+        <br>${listaMensajes[i].texto}</div>
+        `;
+    }
+    caja.scrollTop = caja.scrollHeight;
+
+}
+pintarChat(historialChat);
+
+
+
+
+
+
+
+
 function enviarPrompt(event) {
     // Evitamos que el form recargue la página
     event.preventDefault();
