0969ebaa7f8000cf6b799f54962312b0a3ef6ade
joscanoav <joscanoav@gmail.com>
2026-05-28 19:31:36 +0200

    update

--- 20260527-APIs-Divertidas/02-PokeAPI/pokemon.js
+++ 20260527-APIs-Divertidas/02-PokeAPI/pokemon.js
@@ -13,7 +13,28 @@ btn.addEventListener("click", async () =>{
 
     try {
         const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${termino}`);
-        
-    }
 
-})
+        if(!respuesta.ok) throw new Error("Pokémon no encontrado");
+
+        const pokemon = await respuesta.json();
+
+        //LA FABRICA(Map): Entra un array de objetos raros, salen etiquetas <li> limpias
+        //join("") quita las comas que map() pone por defecto entre los elementos.
+        const estadisticasHTML = pokemon.stats.map(stat =>{
+            return `<li><strong>${stat.stat.name.toUpperCase()}:</strong>
+                    ${stat.base_stat}</li>
+            `;
+        }).join("");
+        const tipos = pokemon.types.map(t => t.type.name).join(", ");
+        out.innerHTML = `
+            <h2>${pokemon.name.toUpperCase()} (#${pokemon.id})</h2>
+            <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" width="150" />
+            <p><strong>Tipos:</strong>${tipos}</p>
+            <ul>
+                ${estadisticasHTML}
+            </ul>
+        `;
+    } catch (error){
+        out.textContent = "❌ Error: No existe ningún Pokémon con ese nombre"
+    }
+});
