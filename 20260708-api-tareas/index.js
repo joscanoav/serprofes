8f0ada9ce0f7640459fbb405ce178aa9601288f1
joscanoav <joscanoav@gmail.com>
2026-07-09 21:41:34 +0200

    add first root

--- 20260708-api-tareas/index.js
+++ 20260708-api-tareas/index.js
@@ -11,6 +11,64 @@ const PORT = 3000;
 //4. Iniciamos el servidor
 //listen() hace que el servidor quede esperando peticiones
 //de los clientes (por ejemplo, desde un navegador)
+
+//================================================
+//MIDDLEWARE
+//================================================
+// Un middleware es una función que se ejecuta antes de
+//llegar a las rutas.
+// express.json()convierte automáticamente los datos
+//enviados en formato JSON en un objeto JavaScript.
+//Gracias a este middleware podremos acceder a :
+// req.body
+//cuando el cliente envíe información mediate POST o PUT.
+app.use(express.json());
+
+//=========================================
+//BASE DE DATOS EN MEMORIA
+//=========================================
+// Simulamos una base de datos utilizando un arreglo
+//IMPORTANTE:
+//Los datos solo existen mientras el servidor está
+//encendido
+//Si detenemos Node.js, toda esta información se pierde
+let tareas = [
+    // Primera tarea
+    {
+        id: 1,
+        titulo: "Aprender Express",
+        completada: false
+    },
+    // Segunda tarea
+    {
+        id:2,
+        titulo: "Estudiar Node.js",
+        completada: true
+    },
+    // Tercera tarea
+    {
+        id:3,
+        titulo: "Practicar Thunder Client",
+        completada:false
+    }
+
+];
+
+//================================
+//RUTA PRINCIPAL
+//================================
+app.get("/", (req, res)=> {
+    res.send("🚀 Bienvenido a la API REST de Tareas");
+})
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => {
     //5. Cuando el servidor se incicia correctamente,
     //mostramos un mensaje en la consola.
